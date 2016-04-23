fb8ef4292ce1e633e26eb9c2eefee72311992c6b
Pedro Costa Neves <neo.nyow@gmail.com>
2016-04-23 11:26:32 -0300

    adding selling items to xur box

--- src/app/javascript/XurBox.jsx
+++ src/app/javascript/XurBox.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import $ from 'jquery';
 
+class XurListItem extends React.Component {
+	render() {
+		return (
+			<li className="modifierListItem boxItem">
+				<img src={'http://bungie.net' + this.props.data.icon} /> 
+				<div className="boxItemText">
+					<p><strong>{this.props.data.itemName}</strong></p> 
+					<p><small>{this.props.data.itemDescription}</small></p>
+				</div>
+			</li>	
+		);
+	}
+};
+
 
 class XurBox extends React.Component {
 	constructor(props) {
@@ -8,6 +22,7 @@ class XurBox extends React.Component {
 		this.state = {
 			title: 'Carregando',
 			backgroundImg: '',
+			items: [],
 			active: false
 		};
 	}
@@ -21,6 +36,7 @@ class XurBox extends React.Component {
 				title: xur.display.advisorTypeCategory,
 				active: xur.status.active,
 				backgroundImg: 'http://bungie.net/img/theme/bungienet/bgs/bg_xuravailable.jpg',
+				items: xur.items
 			});
 		}.bind(this));
 	}
@@ -42,6 +58,14 @@ class XurBox extends React.Component {
 							<h2 className="boxTitle">{this.state.title}</h2>
 							<h3 className="boxSubtitle">{this.state.name}</h3>
 						</div>
+					</div>
+					<div className="boxText">
+						<h4 className="boxSubtitle">Itens a venda</h4>
+						<ul className="boxItems">
+							{this.state.items.map((item, index) => { 
+								return <XurListItem key={index} data={item} />
+							})}
+						</ul>
 					</div>
 				</div>
 			)
