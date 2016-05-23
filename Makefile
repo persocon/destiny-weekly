944c2a84217fe2c68936547183f368e41750d074
Pedro Costa Neves <neo.nyow@gmail.com>
2016-05-23 14:49:50 -0300

    init build-production proccess

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: help watch php-server test test-watch
+.PHONY: help watch dist php-server test test-watch
 
 WEBPACK_CMD = node_modules/.bin/webpack
 WEBPACK_ARGS = --config webpack.config.js --progress --colors --display-error-details
+WEBPACK_ARGS_DIST = --config webpack.config.prod.js --progress --colors --display-error-details
 KARMA_CMD = node_modules/.bin/karma
 KARMA_ARGS =  start karma.config.js
+
+NO_COLOR=\033[0m
+CYAN=\033[36;1m
+CLOUD="☁"
+ARROW="➜"
 
 help:
 	@IFS=$$'\n' ; \
@@ -19,11 +25,24 @@ help:
 watch: ## Starts Webpack Watch
 	$(WEBPACK_CMD) $(WEBPACK_ARGS) --watch
 
+dist: ## Build for Production
+	@echo "${CLOUD} ${CYAN}Copying images${NO_COLOR} ${ARROW}"
+	cp -R src/public/images/ dist/public/images/
+	@echo "${CLOUD} ${CYAN}Copying API${NO_COLOR} ${ARROW}"
+	cp -R src/api/ dist/api/
+	@echo "${CLOUD} ${CYAN}Copying index file${NO_COLOR} ${ARROW}"
+	cp src/index.html dist/index.html
+	@echo "${CLOUD} ${CYAN}Webpack Building${NO_COLOR} ${ARROW}"
+	$(WEBPACK_CMD) $(WEBPACK_ARGS_DIST)
+	@echo "${CLOUD} ${CYAN}Compacting for distribution${NO_COLOR} ${ARROW}"
+	zip -r dist/dist.zip dist/api/ dist/public/ dist/index.html
+
 test: ## Singlerun tests
 	$(KARMA_CMD) $(KARMA_ARGS)
 
 test-watch: ## Starts Test Watch
 	$(KARMA_CMD) $(KARMA_ARGS) --watch
+
 
 php-server: ## Starts PHP-Server
 	cd src && php -S localhost:8888
---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var BUILD_DIR = path.resolve(__dirname, 'dist/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+var config = {
+  entry: APP_DIR + '/javascript/index.jsx',
+  resolve: {
+    alias: {
+        'sinon': 'sinon/pkg/sinon'
+    },
+    root: [
+      // allows us to import modules as if /src was the root.
+      // so I can do: import Comment from 'components/Comment'
+      // instead of:  import Comment from '../components/Comment' or whatever relative path would be
+      path.resolve(__dirname, './src/app')
+    ],
+    // allows you to require without the .js at end of filenames
+    // import Component from 'component' vs. import Component from 'component.js'
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  output: {
+    path: BUILD_DIR+'/javascript/',
+    filename: 'bundle.js'
+  },
+  module : {
+    noParse: [
+        /node_modules\/sinon\//,
+    ],
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      },
+      {
+      	test: /vendor\/.+\.(jsx|js)$/,
+	    loader: 'imports?jQuery=jquery,$=jquery,this=>window'
+	  },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css','sass'])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("../stylesheet/style.css"),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+};
+
+module.exports = config;
