3747b06bc197e67e553ebee9c8c0dbdec29b6fa0
Pedro Costa Neves <neo.nyow@gmail.com>
2016-12-10 12:59:56 -0200

    cleanup logincontainer

--- src/app/javascript/containers/LoginContainer.jsx
+++ src/app/javascript/containers/LoginContainer.jsx
@@ -5,28 +5,6 @@ import Component from '../components/LoginComponent';
 
 const mapStateToProps = (state) => ({ character_list: state.character_list });
 
-const mapDispatchToProps = (dispatch) => ({
-  onSubmit: (event) => {
-    event.preventDefault();
-    const username = event.target.getElementsByClassName('username')[0].value;
-    const platform = event.target.getElementsByClassName('platform')[0].value;
-    if (username) {
-      dispatch(setUser(platform, username));
-      dispatch(setAppScreen('character_list'));
-    } else {
-      const input = event.target.getElementsByClassName('username')[0];
-      input.classList.add('noUserName');
-      input.focus();
-    }
-  },
-  onUsernameChange: (event) => {
-    const input = event.target;
-    if (input.classList.contains('noUserName') && input.value.length >= 1) {
-      input.classList.remove('noUserName');
-    }
-  },
-});
-
 const Container = connect(mapStateToProps, { setUser, setAppScreen })(Component);
 
 export default Container;
